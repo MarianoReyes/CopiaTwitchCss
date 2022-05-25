6905490372702dec842da56c246eda247882d9b5
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2022-05-24 18:43:50 -0600

    mas pruebas

--- src/components/carrusel.js
+++ src/components/carrusel.js
@@ -1,16 +1,13 @@
-import React, {useState} from 'react'
+import React, {StyleSheet, useState} from 'react'
 import '/public/css/carrusel.css'
 
-
 const Carrusel = () => {
-  const [card, setCard] = useState(null);
-  const onCardChange = (event) => {
-    console.log("Card", event);
-  }
+
   
   return (
     <>
     </>
   )
 }
+
 export default Carrusel
