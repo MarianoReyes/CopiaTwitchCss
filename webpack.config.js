a9b2677b29b035e8758df3b1d8bf2a056ed7815c
Mariano Reyes <60166093+MarianoReyes@users.noreply.github.com>
2022-05-26 17:14:39 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,48 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const title = 'Twitch'
-
 export default {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: {
+      directory: 'dist',
     },
-    devServer: {
-        static: {
-            directory: 'dist',
-        },
-        compress:true,
-        port: 9000,
+    compress: true,
+    port: 9000,
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: [{ loader: 'babel-loader' }],
     },
-    module: {
-        rules: [{
-                test: /\.jsx?$/,
-                use: [{loader : 'babel-loader'}],
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(png|jpg|gif|mp3|ttf)$/,
-                use: [{loader: 'file-loader'}]
-            },
-            {
-                test: /\.(svg)$/,
-                use: ['@svgr/webpack']
-            },        
-        ],
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
     },
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    {
+      test: /\.(png|jpg|gif|mp3|ttf)$/,
+      use: [{ loader: 'file-loader' }],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Twitch',
-            template: 'src/index.html',
-            filename: `index.html`,
-            favicon: "public/img/twitch.png",
-            name: 'index',
-            inject: true
-        }),
-    ]
-}
+    {
+      test: /\.(svg)$/,
+      use: ['@svgr/webpack'],
+    },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Twitch',
+      template: 'src/index.html',
+      filename: 'index.html',
+      favicon: 'public/img/twitch.png',
+      name: 'index',
+      inject: true,
+    }),
+  ],
+};
